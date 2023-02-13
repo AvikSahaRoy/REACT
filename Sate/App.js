67fa7983bebc74d0048d4b233ea7f619fbafb14d
Avik Saha Roy <97282235+AvikSahaRoy@users.noreply.github.com>
2023-02-13 10:37:30 +0530

    Create App.js

---
+++ Sate/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import State from "./State"
+
+class App extends Component {
+  render() {
+    return (
+      <State/>
+    );
+  }
+}
+
+export default App;
